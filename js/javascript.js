c2a97f642c4a9462eda93f6c77e78746b054c40c
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-08-25 13:17:25 -0600

    Add form fields, validation, and styling

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,6 @@
 console.log("Hi, Mom!")
+const createAccount = document.querySelector("button#createAccount");
+
+createAccount.addEventListener("click", () => {
+    alert("You should really be more careful about clicking strange buttons.")
+});
